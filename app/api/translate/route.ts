8c85b78e640aba41847cee7bb8caf808cbb22bf2
turtledev <turtledev65@gmail.com>
2023-11-26 21:26:19 +0200

    Refactor the API to use mbart instead of t5

--- app/api/translate/route.ts
+++ app/api/translate/route.ts
@@ -5,7 +5,14 @@ export async function POST(request: NextRequest) {
   const { text, srcLanguage, targetLanguage } = await request.json()
   if (!text.trim() || !srcLanguage.trim() || !targetLanguage.trim()) return NextResponse.json({ error: "Invalid request" }, { status: 400 })
 
-  return inference.translation({ model: "t5-large", inputs: `translate ${srcLanguage} to ${targetLanguage}:  ${text}` })
-    .then(res => NextResponse.json(res.translation_text))
+  return inference.translation({
+    model: "facebook/mbart-large-50-many-to-many-mmt",
+    inputs: text,
+    // @ts-ignore this is ok
+    parameters: {
+      src_lang: srcLanguage,
+      tgt_lang: targetLanguage
+    }
+  }).then(res => NextResponse.json(res.translation_text))
     .catch(error => NextResponse.json({ error: error.message }, { status: 400 }))
 }
