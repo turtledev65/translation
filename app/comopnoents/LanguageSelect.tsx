16da76a6e292ba850289fe3bf95215572ee3d516
turtledev <turtledev65@gmail.com>
2023-11-26 13:16:57 +0200

    Add supported languages

--- app/comopnoents/LanguageSelect.tsx
+++ app/comopnoents/LanguageSelect.tsx
@@ -4,11 +4,13 @@ interface Props {
   className?: string;
 }
 
+const languages = ["English", "French", "Romanian", "German", "Turkish", "Portuguese", "Spanish", "Russian"]
+
 const LanguageSelect = forwardRef<HTMLSelectElement, Props>(({ className }, ref) => {
   return (
-    <select ref={ref} className={`p-2 rounded-lg bg-gray-50 ${className}`}>
-      <option value="">Language</option>
-      <option value="English">English</option>
+    <select ref={ref} defaultValue="" className={`p-2 rounded-lg bg-gray-50 ${className}`}>
+      <option value="" disabled>Language</option>
+      {languages.map(language => <option value={language} key={language}>{language}</option>)}
     </select>
   )
 })
