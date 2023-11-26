289a94c6680b7209e9b3f46821b1d1e12f26c8f7
turtledev <turtledev65@gmail.com>
2023-11-26 14:52:56 +0200

    Improve translation

--- app/api/translate/route.ts
+++ app/api/translate/route.ts
@@ -5,7 +5,7 @@ export async function POST(request: NextRequest) {
   const { text, srcLanguage, targetLanguage } = await request.json()
   if (!text.trim() || !srcLanguage.trim() || !targetLanguage.trim()) return NextResponse.json({ error: "Invalid request" }, { status: 400 })
 
-  return inference.textGeneration({ model: "google/flan-t5-xxl", inputs: `Translate from ${srcLanguage} to ${targetLanguage}:  ${text}` })
-    .then(res => NextResponse.json(res.generated_text))
+  return inference.translation({ model: "t5-large", inputs: `translate ${srcLanguage} to ${targetLanguage}:  ${text}` })
+    .then(res => NextResponse.json(res.translation_text))
     .catch(error => NextResponse.json({ error: error.message }, { status: 400 }))
 }
